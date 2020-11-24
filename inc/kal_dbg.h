20cfe29ab3669568ec1f77bcf0f2f6d032dd2197
j-greffe <jeremie.greffe@gmail.com>
2020-11-25 00:00:35 +0100

    Adapted HAL API.

--- inc/kal_dbg.h
+++ inc/kal_dbg.h
@@ -7,7 +7,7 @@
 
 #define ASSERT(_c) kal_dbg_assert((_c), __FILE__, __LINE__);
 
-void kal_dbg_open(gpio_t* led);
+void kal_dbg_open(gpio_t led);
 void kal_dbg_assert(bool condition, char* file, uint32_t line);
 
 #endif // _KAL_DBG_H_
--- src/kal_dbg.c
+++ src/kal_dbg.c
@@ -2,17 +2,17 @@
 
 static gpio_t g_led;
 
-void kal_dbg_open(gpio_t* led)
+void kal_dbg_open(gpio_t led)
 {
-    g_led.byte = led->byte;
+    g_led = led;
 }
 
 void kal_dbg_assert(bool condition, char* file, uint32_t line)
 {
     if (!condition)
     {
-        hal_gpio_cfg(&g_led, IO_OUT);
-        hal_gpio_set(&g_led);
+        hal_gpio_cfg(g_led, HAL_IO_OUT);
+        hal_gpio_set(g_led);
         while (1);
     }
 }
